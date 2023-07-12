d96a4c43babae344dc0a3e483fda4549cf0f4ece
mopheshi <ndachimya@gmail.com>
2023-07-12 12:46:05 +0100

    ...updated...

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import speech_recognition as sr
 import os
 import csv
 
-from pygame.examples.setmodescale import flag
-
-import getpass_asterisk
 import datetime
 
 # Final global variables
@@ -38,8 +35,7 @@ def inputCommand():
     try:
         print("Recognizing...")
         said = r.recognize_google(audio, language="en-in")
-        print(f"You said...: {said}\n", )
-
+        print(f"You said...: {said}\n")
 
     except Exception as e:
         print(e)
@@ -75,25 +71,17 @@ def intro():
     print("\t\t\t\t------------------------------------")
     print("\t\t\t\t************************************")
     print("\t\t\t\t\tPowered by: Morpheus Softwares")
-    input()
 
 
 intro()
 
 wish()
 
-"""
-eng= "To select English as a language, please press one."
-obj = gTTS(text= eng , slow = False , lang ='en')
-obj.save('eng.mp3')
-speak(eng)
-"""
-
 user = ""
 
 # Inputting and verifying pin and account number
 
-speak("Please, enter your 10-digits account number.")
+speak("Please enter your 10-digit account number.")
 flag1 = 1
 flag2 = 0
 while flag1:
@@ -116,8 +104,8 @@ while flag1:
         tries = 0
 
     while True:
-        accountNumber = input("Enter your 10-digits account number:")
-        pin = 0000
+        accountNumber = input("Enter your 10-digit account number:")
+        pin = "0000"
 
         for i in l:
             if i[3] == accountNumber:
@@ -138,66 +126,53 @@ while flag1:
             print("Account number exists.")
             break
 
-    if accountNumber.isdigit() and len(accountNumber) == 10:  # If accountNumber is equal to 10, then:
+    if accountNumber.isdigit() and len(accountNumber) == 10:
         flag1 = 0
-        speak("Please enter your 4-digits pin number.")
-        print("Please enter your 4-digits pin number.")
-        while flag2 < 3 and flag1 == 1:  # 'and flag1 == 1' as suggested by AskYouTelegram
+        speak("Please enter your 4-digit PIN number.")
+        print("Please enter your 4-digit PIN number.")
+        while flag2 < 3 and flag1 == 0:
             try:
-                pin = getpass_asterisk.getpass()
+                pin = input()
 
             except Exception as error:
                 print("Error: " + error)
                 speak("Error")
 
-            # If pin is equal to 4 and is in the list of pins...
             if pin.isdigit() and len(pin) == 4 and int(pin) in l1:
-                # If the pin is the corresponding pin of the inputted account number, then:
                 if l1.index(int(pin)) == l2.index(int(accountNumber)):
-                    # Check with pin in file, and replace the pin with '*'
-                    flag1 = 0
-                    flag2 = 3
                     print("Accepted!")
                     speak("Accepted!")
+                    flag1 = 0  # Set flag1 to 0 to continue the program execution
+                    break  # Exit the while loop and proceed with the program
                 else:
-                    print("Incorrect pin. Please try again.")
-                    speak("Incorrect pin. Please try again.")
+                    print("Incorrect PIN. Please try again.")
+                    speak("Incorrect PIN. Please try again.")
+                    flag2 += 1
 
-                    flag2 += 1  # From AskYourTelegram
+                if flag2 == 3:
+                    speak("You have exceeded your maximum number of trials.")
+                    exit()
 
-                # Continue to regular code
-
-                # 3 lines commented out below as suggested by AskYourTelegram
-            else:
-                print("Please, enter  your correct pin.")
-                speak("Please, enter your correct pin.")
-                flag2 += 1
-
-            # The line below is shifted out by one indentation as suggested by AskYourTelegram
-            if flag2 == 3:
-                speak("You have exceeded your maximum number of trials.")
-                exit()
 
     else:
-        speak("Please, enter a valid 10-digits account number.")
-        print("Please, enter a valid 10-digits account number.")
+        speak("Please enter a valid 10-digit account number.")
+        print("Please enter a valid 10-digit account number.")
 
 wish()
 speak("Hello and Welcome!")
-speak(user)
+# speak(user)
 
 print(
-    "..........................................................CHOOSE FROM OPTIONS "
-    "BELOW....................................................")
+    "..........................................................CHOOSE FROM OPTIONS BELOW")
 print("")
 
-speak("Please, select from the options below.")
+speak("Please select from the options below.")
 print(
-    "\t\t\t\t\t(1)CASH WITHDRAWAL                                (2)CASH DEPOSIT                                      "
+    "\t\t\t\t\t(1) CASH WITHDRAWAL                                (2) CASH DEPOSIT"
     "              ")
 speak("1. Cash Withdrawal. 2. Cash Deposit")
 print(
-    "\t\t\t\t\t(3)BALANCE INQUIRY                                (4)CHANGE PIN                                        "
+    "\t\t\t\t\t(3) BALANCE INQUIRY                                (4) CHANGE PIN"
     "              ")
 speak("3. Balance Inquiry. 4. Change Pin")
 print(
@@ -293,7 +268,7 @@ elif "Cash deposit" in said.lower() or "Deposit some cash" in said.lower() or "D
             if i[1] == int(pin):
                 while True:
                     if deposit > 20000:
-                        print("Please, enter a valide amount to be deposited.")
+                        print("Please enter a valid amount to be deposited.")
                         speak("The amount you wish to deposit exceeds the limit, please enter a valid amount.")
                         deposit = int(input())
                     else:
@@ -342,76 +317,6 @@ elif "Like to know balance" in said.lower() or "Balance inquiry" in said.lower()
             if i[1] == int(pin) and i[3] == accountNumber:
                 speak(f"Your balance is: {i[2]}")
                 print(f"Your balance is: {i[2]}")
-
-
-# Code fir changing pin
-
-elif "i would like to change my pin" in said.lower() or "change pin" in said.lower():
-
-    speak("You have chosen to change your pin, please enter your old pin.")
-    print("Please enter your old pin: ")
-    tries = 0
-    f = 1
-    while f == 1:
-        oldPin = getpass_asterisk.getpass()
-        if int(oldPin) != int(pin):
-            speak("Your pin does not match, please enter your old pin again.")
-            print("Your pin does not match, please enter your new pin again:")
-            tries = tries + 1
-            if tries == 3:
-                speak("You have exceeded the maximum number of trials.")
-                exit()
-        else:
-            pinTrials = 0
-            while f == 1:
-                speak("Enter your new pin.")
-                print("Enter your new pin:")
-                newPin1 = getpass_asterisk.getpass()
-                if newPin1.isdigit() and len(newPin1) == 4:
-                    speak("Please, re-enter your new pin to confirm.")
-                    print("Please, re-enter your new pin to confirm:")
-                    newPin2 = getpass_asterisk.getpass()
-                    if newPin2.isdigit() and len(newPin2) == 4:
-                        # Pin verification
-                        if newPin1 == newPin2:
-                            with open("Files/Accounts.csv", 'r') as file:
-                                reader = csv.reader(file, delimiter=',')
-                                l = []
-                                for i in reader:
-                                    l.append(i)
-                                for i in l:
-                                    i[1] = int(i[1])
-                                    i[2] = float(i[2])
-                                    # Wrong pin can only be entered at most, 3 times.
-                                    if i[1] == int(oldPin) and i[3] == accountNumber:
-                                        i[1] == int(newPin1)  # More than 3 entries will cause system exit
-                                        f == 0
-
-                                        speak("Your new pin has been set successfully!")
-                                        print("Your new pin has been set successfully!")
-                                        speak(regards)
-                            file = open("Files/Accounts.csv", 'w',
-                                        newline='')
-                            writer = csv.writer(file, delimiter=',')
-                            for i in range(len(l)):
-                                writer.writerow(l[i])
-                            file.close()
-                            exit()
-
-                        else:
-                            speak("The pin does not match, please try again.")
-                            pinTrials = pinTrials + 1
-                            if pinTrials == 3:
-                                speak("You have exceeded the maximum number of trials.")
-                                print("You have exceeded the maximum number of trials.")
-                                exit()
-                            print("The entered pin does not match, please try again.")
-
-    speak("Your new pin has been set successfully!")
-    print("Your new pin has been set successfully!")
-    speak(regards)
-
-    exit()
 
 
 else:
